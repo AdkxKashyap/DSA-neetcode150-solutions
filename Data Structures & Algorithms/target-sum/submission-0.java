14b14fc68da57e9239cb81c585451eadd807d8a4
AdkxKashyap <akashdeepk619@gmail.com>
2026-07-06 12:14:49 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        return helper(nums, target, 0);
+    }
+
+    private int helper(int[] nums, int target, int idx) {
+        if(idx >= nums.length) return target == 0 ? 1 : 0;
+        
+        return helper(nums, target + nums[idx], idx + 1) + helper(nums, target - nums[idx], idx + 1);
+    }
+}
